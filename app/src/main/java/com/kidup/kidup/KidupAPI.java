1ce6247f8b9ce8f4403fa1f52f6399a9f4688cc3
Thanh Mai <t3math00@students.oamk.fi>
2017-05-22 14:24:45 +0300

    adding submit kid_id,let parent set the kid id to the kid's phone

--- app/src/main/java/com/kidup/kidup/KidupAPI.java
+++ app/src/main/java/com/kidup/kidup/KidupAPI.java
@@ -8,18 +8,16 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 /**
  * Created by t3math00 on 5/11/2017.
  */
 
 public interface KidupAPI {
-    @GET("kids/tasks/5910cfd18204a116d433a63f")
-    rx.Observable<List<Task>> getTasks();
+    @GET("kids/tasks/{kid_id}")
+    rx.Observable<List<Task>> getTasks(@Path("kid_id") String kid_id);
 
-    @GET("kids/tasks/registed/5910cfd18204a116d433a63f")
-    rx.Observable<List<Task>> getRegisteredTasks();
-
-    @PUT("kids/tasks/item/task_id")
-    Call<Task> registerTask(@Part("task_id") String id);
+    @PUT("kids/tasks/item/{task_id}")
+    Call<Task> registerTask(@Path(value = "task_id" ,encoded=true) String id);
 }
--- app/src/main/java/com/kidup/kidup/list_tasks.java
+++ app/src/main/java/com/kidup/kidup/list_tasks.java
@@ -1,10 +1,15 @@
 package com.kidup.kidup;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.kidup.kidup.models.Task;
 
@@ -18,6 +23,11 @@ public class list_tasks extends AppCompatActivity {
 
     private RecyclerView rv;
     private List<Task> tasks;
+    private String kid_id;
+    private EditText etId;
+    private Button btnSubmit;
+    public static final String PREFS_NAME = "MyPrefsFile";
+
 
 
     private KidupAPI mainApi;
@@ -25,24 +35,61 @@ public class list_tasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_tasks);
+        etId = (EditText)findViewById(R.id.et_id);
+
+        /* Get time from system */
+        SharedPreferences sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        Log.d("mai thanh tung", " id saved");
+
+        editor.commit();
+        btnSubmit = (Button)findViewById(R.id.btn_submit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("maitung", "Clicked");
+                String keyword = etId.getText().toString();
+                Log.e("maitung", "key word : " + keyword);
+                SharedPreferences sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                Log.d("maitung", " id saved");
+                editor.putString("kid_id",keyword);
+                editor.commit();
 
 
 
-        mainApi = API.get().create(KidupAPI.class);
-        init();
+                kid_id = sharedPref.getString("kid_id","ditme");
+                Log.e("maitung", "kid_id" + kid_id );
+                init(kid_id);
+            }
+        });
+        if( sharedPref.getString("kid_id",null) != ""){
+            kid_id = sharedPref.getString("kid_id",null);
+
+            Log.e("maitung", "check sharedpref" + kid_id);
+//
+        }
+        if (kid_id != null){
+            btnSubmit.setVisibility(View.INVISIBLE);
+            etId.setVisibility(View.INVISIBLE);
+            mainApi = API.get().create(KidupAPI.class);
+            init(kid_id);
+        }
     }
 
-    private void  init(){
+    private void  init(String kid_id){
         rv = (RecyclerView) findViewById(R.id.rv_list);
         rv.setLayoutManager(new LinearLayoutManager(this));
-        getTask();
-        Log.d("mai thanh tung", "init: ");
+        Log.d("maitung", "init: " + kid_id);
+        getTask(kid_id);
     }
 
 
-    private void getTask() {
+    private void getTask(String kid_id) {
 
-        mainApi.getTasks()
+        mainApi.getTasks(kid_id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<List<Task>>() {
@@ -58,12 +105,9 @@ public class list_tasks extends AppCompatActivity {
 
                     @Override
                     public void onNext(List<Task> tasks) {
-//                        TextView textView = (TextView)findViewById(R.id.);
                         rv.setAdapter(new Adapter(tasks));
                         rv.getAdapter().notifyDataSetChanged();
-                        Log.e("Task thu nhat ", tasks.get(0).description);
-                        Log.e("Task thu hai ", tasks.get(1).description);
-//                        textView.setText(tasks.get(0).description);
+
 
                     }
                 });
